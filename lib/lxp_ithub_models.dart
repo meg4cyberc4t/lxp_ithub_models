1e055ef51403837534da8c14f438f60460c3550b
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2022-09-15 18:28:45 +0300

    Update lxp_ithub_models.dart

--- lib/lxp_ithub_models.dart
+++ lib/lxp_ithub_models.dart
@@ -3,5 +3,4 @@ library lxp_ithub_models;
 export 'package:lxp_ithub_models/lxp_ithub_models.dart';
 
 export 'models/models.dart';
-export 'models/models.mapper.g.dart';
 export 'utils/string_html_extension.dart';
