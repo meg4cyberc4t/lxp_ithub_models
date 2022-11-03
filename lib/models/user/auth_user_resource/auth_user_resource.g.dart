6fe43def4ef93b8f7a99fa6497673082c241d789
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2022-11-03 13:58:48 +0300

    update auth_user_resource

--- lib/models/user/auth_user_resource/auth_user_resource.g.dart
+++ lib/models/user/auth_user_resource/auth_user_resource.g.dart
@@ -16,8 +16,6 @@ _$_AuthUserResource _$$_AuthUserResourceFromJson(Map<String, dynamic> json) =>
       photoSrc: json['photo_src'] as String?,
       phone: json['phone'],
       photoSrcSmall: json['photo_src_small'] as String,
-      isSkillfolioReg: json['is_skillfolio_reg'] as bool,
-      isOnline: json['is_online'] as bool,
       createdAt: json['created_at'] == null
           ? null
           : DateTime.parse(json['created_at'] as String),
@@ -30,9 +28,13 @@ _$_AuthUserResource _$$_AuthUserResourceFromJson(Map<String, dynamic> json) =>
       children: (json['children'] as List<dynamic>)
           .map((e) => BaseUserResource.fromJson(e as Map<String, dynamic>))
           .toList(),
+      isCurator: json['is_curator'] as bool,
+      isDistant: json['is_distant'] as bool,
       isMember: json['is_member'] as bool,
+      isOnline: json['is_online'] as bool,
       isModerator: json['is_moderator'] as bool,
       isParent: json['is_parent'] as bool,
+      isSkillfolioReg: json['is_skillfolio_reg'] as bool,
       groupsCount: json['groups_count'] as int,
       subjectsCount: json['subjects_count'] as int,
     );
@@ -47,15 +49,17 @@ Map<String, dynamic> _$$_AuthUserResourceToJson(_$_AuthUserResource instance) =>
       'photo_src': instance.photoSrc,
       'phone': instance.phone,
       'photo_src_small': instance.photoSrcSmall,
-      'is_skillfolio_reg': instance.isSkillfolioReg,
-      'is_online': instance.isOnline,
       'created_at': instance.createdAt?.toIso8601String(),
       'updated_at': instance.updatedAt?.toIso8601String(),
       'groups': instance.groups,
       'children': instance.children,
+      'is_curator': instance.isCurator,
+      'is_distant': instance.isDistant,
       'is_member': instance.isMember,
+      'is_online': instance.isOnline,
       'is_moderator': instance.isModerator,
       'is_parent': instance.isParent,
+      'is_skillfolio_reg': instance.isSkillfolioReg,
       'groups_count': instance.groupsCount,
       'subjects_count': instance.subjectsCount,
     };
