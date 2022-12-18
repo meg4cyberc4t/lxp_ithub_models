c645effead5276f4545ac6d76f98bc3e1d7979a5
meg4cyberc4t <66907532+meg4cyberc4t@users.noreply.github.com>
2022-12-18 13:49:23 +0300

    Add passed_status_dto

---
+++ lib/models/dto/passed_status_set_dto/passed_status_set_dto.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'passed_status_set_dto.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_PassedStatusSetFto _$$_PassedStatusSetFtoFromJson(
+        Map<String, dynamic> json) =>
+    _$_PassedStatusSetFto(
+      passedStatusId: json['passed_status_id'] as int,
+    );
+
+Map<String, dynamic> _$$_PassedStatusSetFtoToJson(
+        _$_PassedStatusSetFto instance) =>
+    <String, dynamic>{
+      'passed_status_id': instance.passedStatusId,
+    };
